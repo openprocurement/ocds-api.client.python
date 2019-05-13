b6b2455fa6f3b03d79fd6bfc744d456b21314dc8
Taras Vaskiv <codertarasvaskiv@quintagroup.org>
2019-05-13 11:27:19 +0300

    Update tests

--- ocds_client/tests/test_workers.py
+++ ocds_client/tests/test_workers.py
@@ -3,20 +3,15 @@ import os
 
 from .utils import Response
 from gevent import spawn, sleep
-from unittest.mock import patch
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
 
 from ocds_client.sync import SyncClient
 
 CWD = os.getcwd()
 
 
-@patch('ocds_client.templates.Session')
-def test_forward_worker(session):
-
-    with open(f'{CWD}/ocds_client/tests/data/records-10.json', 'r') as f:
-        data = f.read()
-    session().request.return_value = Response(data, 200)
-
+class TestWorkers(TestCase):
     host = 'http://localhost'
     resource = 'record'
     params = {'size': '10', 'ordering': 'desc'}
@@ -26,51 +21,85 @@ def test_forward_worker(session):
         'up_wait_sleep_min': 1,
         'queue_size': 101
     }
-    sync_client = SyncClient(host, resource, params=params, retrievers_params=retrievers_params)
 
-    def start_test_client():
+    @classmethod
+    def setUpClass(cls):
+        with open(f'{CWD}/ocds_client/tests/data/records-10.json', 'r') as f:
+            cls.data = f.read()
+
+    def setUp(self):
+        self.sync_client = SyncClient(self.host, self.resource, params=self.params,
+                                 retrievers_params=self.retrievers_params)
+
+    @staticmethod
+    def start_test_client(sync_client):
         sync_client.init_client()
         for record in sync_client.get_resource_items():
             assert set(record.keys()) == {'ocid', 'compiledRelease', 'releases'}
 
-    spawn(start_test_client)
-    sleep(3)
-    assert sync_client.backward_worker.ready() == False
-    assert sync_client.backward_worker.exit_successful == False
-    assert sync_client.forward_worker.ready() == False
-    assert sync_client.forward_worker.exit_successful == False
+    @patch('ocds_client.templates.Session')
+    def test_forward_worker(self, session):
 
-    # emulate exception in backward worker, check restart_sync was called
-    forward_worker = sync_client.forward_worker
-    backward_worker = sync_client.backward_worker
-    backward_worker.kill(exception=KeyError())
-    sleep(3)
-    assert sync_client.forward_worker != forward_worker  # restart_sync was called
-    assert sync_client.backward_worker != backward_worker  # restart_sync was called
-    assert forward_worker.exit_successful == False
-    assert backward_worker.exit_successful == False
-    assert forward_worker.ready() == True
-    assert backward_worker.ready() == True
+        session().request.return_value = Response(self.data, 200)
 
-    # emulate exception in forward worker, check restart_sync was called
-    forward_worker = sync_client.forward_worker
-    backward_worker = sync_client.backward_worker
-    forward_worker.kill(exception=KeyError())
-    sleep(3)
-    assert sync_client.forward_worker != forward_worker  # restart_sync was called
-    assert sync_client.backward_worker != backward_worker  # restart_sync was called
-    assert forward_worker.exit_successful == False
-    assert backward_worker.exit_successful == False
-    assert forward_worker.ready() == True
-    assert backward_worker.ready() == True
+        spawn(self.start_test_client, self.sync_client)
+        sleep(3)
+        self.assertEquals(self.sync_client.backward_worker.ready(), False)
+        self.assertEquals(self.sync_client.backward_worker.exit_successful, False)
+        self.assertEquals(self.sync_client.forward_worker.ready(), False)
+        self.assertEquals(self.sync_client.forward_worker.exit_successful, False)
 
-    # emulate backward worker finished sync
-    json_data = json.loads(data)
-    json_data['records'] = []
-    data = json.dumps(json_data)
-    session().request.return_value = Response(data, 200)
-    sleep(3)
-    assert sync_client.backward_worker.ready() == True
-    assert sync_client.backward_worker.exit_successful == True
-    assert sync_client.forward_worker.ready() == False
-    assert sync_client.forward_worker.exit_successful == False
+        # emulate backward worker finished sync
+        json_data = json.loads(self.data)
+        json_data['records'] = []
+
+        data_without_records = json.dumps(json_data)
+        session().request.return_value = Response(data_without_records, 200)
+        sleep(3)
+        self.assertEquals(self.sync_client.backward_worker.ready(), True)
+        self.assertEquals(self.sync_client.backward_worker.exit_successful, True)
+        self.assertEquals(self.sync_client.forward_worker.ready(), False)
+        self.assertEquals(self.sync_client.forward_worker.exit_successful, False)
+
+    @patch('ocds_client.templates.Session')
+    def test_workers_with_backward_exception(self, session):
+
+        session().request.return_value = Response(self.data, 200)
+
+        # start sync
+        spawn(self.start_test_client, self.sync_client)
+        sleep(3)
+        self.assertEquals(self.sync_client.backward_worker.ready(), False)
+        self.assertEquals(self.sync_client.backward_worker.exit_successful, False)
+        self.assertEquals(self.sync_client.forward_worker.ready(), False)
+        self.assertEquals(self.sync_client.forward_worker.exit_successful, False)
+
+        restart_sync_mock = MagicMock(return_value='weird value')
+        self.sync_client.restart_sync = restart_sync_mock
+
+        # emulate exception in backward worker, check restart_sync was called
+        backward_worker = self.sync_client.backward_worker
+        backward_worker.kill(exception=KeyError())
+        sleep(3)
+        self.sync_client.restart_sync.assert_called_with()
+
+    @patch('ocds_client.templates.Session')
+    def test_workers_with_forward_exception(self, session):
+
+        session().request.return_value = Response(self.data, 200)
+        spawn(self.start_test_client, self.sync_client)
+
+        sleep(3)
+        self.assertEquals(self.sync_client.backward_worker.ready(), False)
+        self.assertEquals(self.sync_client.backward_worker.exit_successful, False)
+        self.assertEquals(self.sync_client.forward_worker.ready(), False)
+        self.assertEquals(self.sync_client.forward_worker.exit_successful, False)
+
+        restart_sync_mock = MagicMock(return_value='weird value')
+        self.sync_client.restart_sync = restart_sync_mock
+
+        # emulate exception in forward worker, check restart_sync was called
+        forward_worker = self.sync_client.forward_worker
+        forward_worker.kill(exception=KeyError())
+        sleep(3)
+        self.sync_client.restart_sync.assert_called_with()
